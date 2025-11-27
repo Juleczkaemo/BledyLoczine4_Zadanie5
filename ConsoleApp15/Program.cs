4ba91c126a80e70b26d76a9fc16223bd890b802f
4Mgr1 <4Mgr1@DESKTOP-EHD8S3P>
2025-11-27 09:20:47 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 3;
+        int wynik = 2;
+
+        for (int i = 1; i < n; i++)
+        {
+            wynik *= i;
+        }
+
+        Console.WriteLine($"2^{n} = {wynik}");
+    }
+}
