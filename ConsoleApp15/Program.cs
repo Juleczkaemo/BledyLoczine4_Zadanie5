d44a3e77fa5e55e1bf2732ebea688812bdb6ee28
4Mgr1 <4Mgr1@DESKTOP-EHD8S3P>
2025-12-02 11:47:05 +0100

    zadanie z komentarzem

--- ConsoleApp15/Program.cs
+++ ConsoleApp15/Program.cs
@@ -1,15 +1,17 @@
 ﻿using System;
+using System.Reflection;
 
 class Program
 {
     static void Main()
     {
         int n = 3;
+        int m = 2; // dodanie zmiennej, która przechowuje wartość początkową która ma być mnożnikiem (w tym wypadku 2)
         int wynik = 2;
 
         for (int i = 1; i < n; i++)
         {
-            wynik *= i;
+            wynik *= m;
         }
 
         Console.WriteLine($"2^{n} = {wynik}");
